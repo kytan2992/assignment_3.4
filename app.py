d11cffe27c135d31727bb12ce0aba137eb30b13a
kytan2992 <kytan2992@gmail.com>
2025-04-09 21:47:10 +0800

    update commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, KE YANG!</p>"
-
-if __name__ == '__main__':
+from flask import Flask 
+ 
+app = Flask(__name__) 
+ 
+@app.route("/") 
+def hello_world(): 
+    return "<p>Hello, Norman!</p>" 
+ 
+if __name__ == '__main__': 
     app.run(host="0.0.0.0", port=8080)
